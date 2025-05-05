34511772fff73d5fde0f299fdb951ec8abeb9aef
honey nagpal <honeyynagpal11@gmail.com>
2025-05-06 03:07:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 
-# Initialize the FastAPI application
 app = FastAPI()
 
-# Define the /sayHello route
 @app.get("/sayHello")
 def say_hello():
     return {"message": "Hello User"}
